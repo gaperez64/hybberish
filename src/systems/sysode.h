8e7414bed0ac36f74cb7f964afb0f145d6f15c1a
Guillermo A. Perez <gaperez64@gmail.com>
2023-04-09 21:30:40 +0200

    now with clang format checks too

--- src/systems/sysode.h
+++ src/systems/sysode.h
@@ -1,18 +1,18 @@
 enum ODEExpType {
-    NUM,
-    ADD_OP,
-    SUB_OP,
-    MUL_OP,
-    DIV_OP,
-    EXP_OP,
-    NEG,
-    VAR,
-    FUN,
+  NUM,
+  ADD_OP,
+  SUB_OP,
+  MUL_OP,
+  DIV_OP,
+  EXP_OP,
+  NEG,
+  VAR,
+  FUN,
 };
 
 typedef struct ODEExpTree {
-    char* data;
-    ODEExpType type;
-    struct ODEExpTree* left;
-    struct ODEExpTree* right;
+  char *data;
+  ODEExpType type;
+  struct ODEExpTree *left;
+  struct ODEExpTree *right;
 } ODEExpTree;
