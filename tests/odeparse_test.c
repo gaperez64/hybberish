efff480ce495c13e718684113273b2950dbf754e
Guillermo A. Perez <gaperez64@gmail.com>
2023-04-19 21:43:37 +0200

    aagaaaaain

--- tests/odeparse_test.c
+++ tests/odeparse_test.c
@@ -6,7 +6,7 @@
 #include <string.h>
 
 int main(int argc, char *argv[]) {
-  /* to avoid silly warnings about unused parameters */
+  /* avoid silly warnings about unused parameters */
   (void)argc;
   (void)argv;
 
