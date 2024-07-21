62e4ba58c6486881fa85d825257e8453978aca3f
ELthomasoPostfix <thomas.gueutal@student.uantwerpen.be>
2024-07-21 13:11:53 +0200

    Add basic float-based Interval arithmetic.
    This commit contains only source code,
     not the meson build config and tests.
     This is done intentionally to make the
     commits more modular and self-contained.
    
    The current implementation is fully float
     based and provides limited interval
     arithmetic as defined in the Flow* thesis.
    
    The interface is the following:
      * A constructor 'newInterval' to enforce
        well formedness
      * binary operations: +,-,*,/,=,in where
        'in' refers to 'I1 subset of I2' for
        intervals, not checking if a float is
        in an interval
      * interval properties: Width(I), Mid(I),
        Mag(I) and a degeneracy test.
    ==> Note that some operations require an
      epsilon to allow flexibility in approximation
      errors for equality comparisons.

---
+++ src/taylormodel/interval.h
@@ -0,0 +1,46 @@
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+
+
+
+/* A closed interval I = [a, b], so that a <= b. */
+typedef struct Interval {
+  float left;
+  float right;
+} Interval;
+
+
+
+/* Create a new interval that satisfies its definition. */
+Interval newInterval(float left, float right);
+
+void printInterval(Interval *source, FILE *where);
+
+
+/*
+  Interval operations.
+*/
+Interval addInterval(Interval *left, Interval *right);
+Interval subInterval(Interval *left, Interval *right);
+Interval mulInterval(Interval *left, Interval *right);
+Interval divInterval(Interval *left, Interval *right);
+bool eqInterval(Interval *left, Interval *right, float epsilon);
+bool inInterval(Interval *left, Interval *right);
+
+
+
+/*
+  Properties of an Interval I = [a, b].
+*/
+/* W(I) = b - a */
+float intervalWidth(Interval *source);
+/* Mid(I) = (a + b) / 2 */
+float intervalMidpoint(Interval *source);
+/* Mag(I) = max{|a|, |b|} */
+float intervalMagnitude(Interval *source);
+
+/* Interval I is degenerate if a = b.
+  Due to precision concerns, an epsilon must be specified. */
+bool intervalIsDegenerate(Interval *source, float epsilon);
