4a9816c69e91fd0f6de94257080913d3d509675c
Guillermo A. Perez <gaperez64@gmail.com>
2024-10-23 15:14:05 +0200

    fixed, now runs

--- flowstar.jl
+++ flowstar.jl
@@ -1,8 +1,9 @@
-using TaylorSeries
+using TaylorSeries  # IntervalArithmetic
 
 # Assumes the last variable is t
 function tay_poly(f, k)
     vars = get_variables()
+    t = vars[end]
     # Let's pad f with a 1 at the end for t
     fp1 = copy(f)
     push!(fp1, 1)
@@ -10,7 +11,7 @@ function tay_poly(f, k)
     # FIXME: We're cheating to obtain a zero with same order as the other
     # variables
     val0 = copy(vars)
-    val0[end] = vars[1] - vars[1]
+    val0[end] = t - t
     # Prepare a first lie derivative and the result
     g = copy(vars)
     deleteat!(g, length(vars))
@@ -18,17 +19,16 @@ function tay_poly(f, k)
     res = copy(g)
     
     for i = 1:k
-        println("g = $g, vars = $vars")
+        println("g = $g")
         g = TaylorSeries.jacobian(g, vars) * fp1
         term = map((h) -> evaluate(h, val0) * t^i * (1 / factorial(i)), g)
-        println("term = $term")
         res += term
     end
     
     return res
 end
 
-vars = set_variables("x y t", order=5)
+vars = set_variables("x y t", order=4)
 f = [1 + vars[2], -vars[1]^2]
-p = tay_poly(f, 5)
+p = tay_poly(f, 4)
 println("poly from new TM = $p")
