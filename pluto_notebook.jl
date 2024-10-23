4a3b4c660b49b518aecf44cfa7f0b65825c59089
Kasper Engelen <kasper.engelen@hotmail.com>
2024-10-23 19:26:18 +0200

    Added minimal notebook

---
+++ pluto_notebook.jl
@@ -0,0 +1,24 @@
+### A Pluto.jl notebook ###
+# v0.19.47
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0c67b6fe-9163-11ef-019e-333f0ff3dc9d
+md"""
+Text can be added in Markdown format. Rendering is done with the "play" button, or CMD+S.
+"""
+
+# ╔═╡ 7066cb63-a64d-40e8-8a0e-684278b24e98
+# add some code
+println("Hello world")
+
+# ╔═╡ ef7165a0-b7ca-4836-b8d3-7d557555efb5
+md"""
+Be sure to check out the "Live Docs" and "Status" buttons as well.
+"""
+
+# ╔═╡ Cell order:
+# ╠═0c67b6fe-9163-11ef-019e-333f0ff3dc9d
+# ╠═7066cb63-a64d-40e8-8a0e-684278b24e98
+# ╠═ef7165a0-b7ca-4836-b8d3-7d557555efb5
