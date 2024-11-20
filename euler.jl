d9d7fa0897fafaa675d2776ba372fa8a13e019dc
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2024-11-20 11:57:51 +0100

    Fix: do NOT shift the 3.3.11 euler ODEs.

--- euler.jl
+++ euler.jl
@@ -24,8 +24,8 @@ end
 function ode3311!(du, u, p, t)
 	# FIXME: offset x by +5 and y by +2, since the TaylorModels
 	# library requires 0 to be in the domains and such.
-    du[1] = 1.5u[1] - u[1] * u[2] + 5
-    du[2] = -3 * u[2] + u[1] * u[2] + 2
+    du[1] = 1.5u[1] - u[1] * u[2]
+    du[2] = -3 * u[2] + u[1] * u[2]
 end
 
 function trucktrailer!(du, u, p, t)
