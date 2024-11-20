51bebd00cde89a95eb4c3baae2864b695db0f364
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2024-11-20 12:57:34 +0100

    Shift the 3.3.11 ODEs and domains.
    The domains of example 3.3.11 are not
     centered on the origin. So shift the
     domains so that they are, and counter
     shift the ODEs so that the overall
     influence is neutral.
    
    Add the relevant functions in
     'ode_shifting.jl'.

--- notebook_3_3_11.jl
+++ notebook_3_3_11.jl
@@ -18,6 +18,7 @@ begin
 	include("plotting.jl")
 	include("euler.jl")
 	include("tm_integration.jl")
+	include("ode_shifting.jl")
 end
 
 # ╔═╡ 9ef48c3e-a529-11ef-3721-1330db209c3b
@@ -37,6 +38,13 @@ md"""
 We first set up the environment, by including the necessary packages and files, etc.
 """
 
+# ╔═╡ 259509ae-a32b-4039-8bd0-d18a5dec163f
+md"""
+## ODE shift helper functions
+
+We define some helper functions that aid in shifting/translating the ODEs, so that the initial hyperrectangle is centered on the origin.
+"""
+
 # ╔═╡ b879632b-4457-4f0f-b3d4-69ae5eb6e59f
 md"""
 ## Configure TM integration
@@ -47,26 +55,25 @@ We now set up the hardcoded inputs. The comments in the markdown cells will be k
 # ╔═╡ f9d36075-e6bd-49bf-af79-0ce17108f282
 begin
     # Full TM integration, based on Example 3.3.11
-    k = 3   # The TM arithmetic and truncation order
+    k = 5   # The TM arithmetic and truncation order
     NR_CONTRACTIVENESS_TRIES = 5
     NR_REFINEMENTS           = 1
     SCALE                    = 2.0
     TIME_STEP_SIZE = 0.02
     TIME_STEP_SIZE_EPS = 2.0e-8  # The minimum time step-size
-    time_horizon = 0.7     # The finite time horizon
+    time_horizon = 0.8     # The finite time horizon
     vars = set_variables("x y t", order=k)
     vars_no_t = get_variable_names()[1:end-1]
 
-	# FIXME: offset x by +5 and y by +2, since the TaylorModels
-	# library requires 0 to be in the domains and such.
-    dom = IntervalBox([
-		-1..1,   # x
-		-1..1,   # y
-    	0..0.02 # t
+    dom::IntervalBox = IntervalBox([
+		4.9..5.1,   # x
+		1.9..2.1,   # y
+    	0..0.02  # t
 	])
-    vars_tms = tm_initial_set(dom)
-    
-    
+
+	# Shift the domains, and replace all variables by shifted variables.
+	dom, vars_tms, shift_offsets = shifted_vars_tms(dom)
+
     # FIXME: Bumping up the order to avoid assertion errors
     # during TM arithmetic.
     old_order = get_order()
@@ -81,11 +88,9 @@ begin
     #   f[2] = -3y + xy + [0, 0]
     # where the Lagrange remainder is always zero, since
     # the vector field contains only polynomial terms
-	# FIXME: offset x by +5 and y by +2, since the TaylorModels
-	# library requires 0 to be in the domains and such.
     f = [
-		1.5 * vars_tms[1] - vars_tms[1] * vars_tms[2] + 5,
-		-3 * vars_tms[2]  + vars_tms[1] * vars_tms[2] + 2
+		1.5 * vars_tms[1] - vars_tms[1] * vars_tms[2],
+		-3 * vars_tms[2]  + vars_tms[1] * vars_tms[2]
 	]
     # Initial remainder estimate J, a hyperrectangle
     J = fill(-0.1..0.1, length(f))
@@ -93,7 +98,7 @@ begin
     # TODO: Reset to old order.
     vars = set_variables(get_variable_string(), order=old_order)
 
-	f # Pluto cell output
+	dom, vars_tms, f # Pluto cell output
 end
 
 # ╔═╡ 45ede61b-c358-44c0-a096-9315fd0524fa
@@ -137,6 +142,9 @@ begin
 	initial_boxes = map((Xi) -> interval_initial_set(Xi, dom), initial_sets)
 	# FIXME: Assume t is the last variable, and drop its dummy vector element.
 	initial_boxes = map((Bij) -> Bij[1:end-1], initial_boxes)
+	# Undo the shift applied to the ODEs and domains, to obtain the
+	# boxes of the true ODEs and domains.
+	initial_boxes = unshift_boxes(initial_boxes, shift_offsets)
 	# Typecast the initial boxes for cleanliness of the source code.
 	initial_boxes = map((Bij) -> IntervalBox(Bij...), initial_boxes)
 end
@@ -147,10 +155,20 @@ begin
 
 	USE_LOCAL_HORIZON = false 	# Boolean flag, 
 	known_boxes = copy(get_clean_boxes()) 	# Check intersection between computed and known boxes
+	known_boxes = map((_) -> known_boxes[1], eachindex(initial_boxes))
 
 	# Compare as many known VS computed boxes as possible, so that
 	# you can always generate some plot even if the lengths mismatch.
 	known_boxes = known_boxes[1:min(length(known_boxes), length(initial_boxes))]
+
+	# FIXME: just ignore known boxes since it's dummy
+	empty!(known_boxes)
+
+	length_diff = length(initial_boxes) - length(known_boxes)
+	if length_diff > 0
+		padding = map((_) -> IntervalBox(zero(initial_boxes[1])), 1:length_diff)
+		known_boxes = vcat(known_boxes, padding)
+	end
 end
 
 # ╔═╡ 84afb4c5-c7b4-40fd-bb1a-37ab44994eb9
@@ -173,13 +191,14 @@ Also plot the approximate solution of the ODE obtained by **forward Euler** as a
 # ╔═╡ 87acf7ba-fd57-40e0-b9e7-53ff97379a1f
 begin
 	euler_init_state = [
-		0.0; # x
-		0.0  # y
+		5.0; # x
+		2.0  # y
 	]
 	# Generate the composed plot, which also does euler
-	plt_composed, tseries_, vseries_ = plot_vars_against_time(initial_boxes, known_boxes, time_horizon, step_sizes, vars_no_t, USE_LOCAL_HORIZON, ode332!, euler_init_state)
+	plt_composed, tseries_, vseries_ = plot_vars_against_time(initial_boxes, known_boxes, time_horizon, step_sizes, vars_no_t, USE_LOCAL_HORIZON, ode3311!, euler_init_state)
 
 	plt_composed # Pluto cell output
+	# plot(plt_composed, xlims=(-10, 10), ylims=(-10, 10))
 end
 
 # ╔═╡ Cell order:
@@ -187,6 +206,7 @@ end
 # ╟─f4f8b389-7090-4084-8d57-3e0e2907a27b
 # ╠═3b649995-2db2-4e42-ad58-a5e95460d574
 # ╠═33edc776-1ada-4fec-896a-426cd4614d74
+# ╟─259509ae-a32b-4039-8bd0-d18a5dec163f
 # ╟─b879632b-4457-4f0f-b3d4-69ae5eb6e59f
 # ╠═f9d36075-e6bd-49bf-af79-0ce17108f282
 # ╟─45ede61b-c358-44c0-a096-9315fd0524fa
---
+++ ode_shifting.jl
@@ -0,0 +1,53 @@
+using IntervalArithmetic
+using TaylorSeries
+
+"""Compute the shifted domains and shifted variables with which to construct the ODEs.
+
+    Given the hyperrectangle B = (b1, ..., bn).
+    Then bi is domain of ODE variable xi. If we center the hyperrectangle B
+    on the origin, C = (c1, ..., cn) where ci = bi - mid(bi), then we must shift
+    the variables in the opposite direction to counteract this. So the
+    shifted variables are yi = xi + mid(bi).
+
+    @param[in] dom The domains of all variables, including time t.
+    @return (
+        C = (c1, ..., cn) where ci is an interval,
+        [(y1, [0, 0]), ..., (yn, [0, 0])] where (y1, [0, 0]) is a Taylor model,
+        [mid(b1), ..., mid(bn)]
+    )
+"""
+function shifted_vars_tms(dom::IntervalBox)
+	# Center the initial hyper rectangle on the origin.
+	# This applies a SUBTRACTION to the domains.
+	offsets::Vector{Float64} = map((domj) -> mid(domj), dom)
+	# FIXME: Assume the last component is a dummy interval for time t
+	offsets[end] = 0
+	dom_shifted = dom - IntervalBox(offsets...)	# The SUBTRACTION
+
+	# Construct the identity TM for each variable.
+    vars_tms = tm_initial_set(dom_shifted)
+
+	# Shift the ODEs to compensate for the shifted hyper rectangle
+	# This applies an ADDITION to the ODEs that compensates the
+	# previous SUBTRACTION. The overall effect of shifting the domains
+	# one way and the ODEs the opposite way is NEUTRAL.
+	# ==> The ODEs are built using the vector of var TMs, so just
+	#     modify that vector
+	cte_order = get_order() # We MUST use the current order to avoid accidental truncation
+	shifted_vars_tay = map((o) -> TaylorN(o, cte_order), offsets)
+	vars_tms = vars_tms + shifted_vars_tay # The ADDITION
+
+	return dom_shifted, vars_tms, offsets
+end
+
+"""Translate the given boxes by the given vector.
+
+    @return The translated boxes
+"""
+function unshift_boxes(boxes::Vector{Vector{Interval{Float64}}}, offsets::Vector{Float64})
+	# FIXME: Assume the last component corresponds to time t.
+	offsets = offsets[1:(end-1)]
+
+	# Undo the SUBTRACTION that the shifting applied to the domain.
+	return map((box) -> box + offsets, boxes)
+end
