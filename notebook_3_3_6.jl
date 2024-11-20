cd070b6ab68ccd242fce7f18bc138331b7ada0cf
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2024-11-20 12:15:33 +0100

    Update the 3.3.6 notebook TM integration config.
    * Change TM integration config:
      * set k from 3 to 5
      * set time horizon from 0.8 to 2.0
    * Padd the known boxes to be as long as the
      computed initial boxes

--- notebook_3_3_6.jl
+++ notebook_3_3_6.jl
@@ -48,13 +48,13 @@ We now set up the hardcoded inputs. The comments in the markdown cells and the i
 # ╔═╡ e3aa973e-2a96-45c7-bcb4-40758cc687be
 # Full TM integration, based on Example 3.3.2 and Example 3.3.6
 begin
-	k = 3   # The TM arithmetic and truncation order
+	k = 5   # The TM arithmetic and truncation order
 	NR_CONTRACTIVENESS_TRIES = 5
 	NR_REFINEMENTS           = 1
 	SCALE                    = 2.0
 	TIME_STEP_SIZE = 0.02
 	TIME_STEP_SIZE_EPS = 2.0e-8  # The minimum time step-size
-	time_horizon = 0.8     # The finite time horizon
+	time_horizon = 2.0     # The finite time horizon
 	vars = set_variables("x y t", order=k)
 	vars_no_t = get_variable_names()[1:end-1]
 	dom = IntervalBox([-1..1,      # x
@@ -170,6 +170,14 @@ begin
 	# Compare as many known VS computed boxes as possible, so that
 	# you can always generate some plot even if the lengths mismatch.
 	known_boxes = known_boxes[1:min(length(known_boxes), length(initial_boxes))]
+
+	length_diff = length(initial_boxes) - length(known_boxes)
+	if length_diff > 0
+		padding = map((_) -> IntervalBox(zero(initial_boxes[1])), 1:length_diff)
+		known_boxes = vcat(known_boxes, padding)
+	end
+
+	known_boxes # Pluto cell output
 end
 
 # ╔═╡ 244cb2cc-323b-43b9-a7fd-4ca2a1bff0d6
