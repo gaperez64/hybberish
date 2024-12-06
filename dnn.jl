d266d8880f1ecb4a744b71290563fd2299a1f850
Guillermo A. Perez <gaperez64@gmail.com>
2024-12-06 20:44:58 +0100

    fixed

--- dnn.jl
+++ dnn.jl
@@ -4,7 +4,7 @@ using YAML
 function load_dnn_fun(yml_filename)
     data = YAML.load_file(yml_filename)
     offsets = [data["offsets"][key] for key in 1:length(data["offsets"])]
-    weights = [stack(data["weights"][key], dims=1)
+    weights = [transpose(stack(data["weights"][key], dims=1))
                for key in 1:length(data["weights"])]
     zs = [zeros(length(offsets[key])) for key in 1:length(data["offsets"])]
     funs = [x -> max.(weights[i] * x + offsets[i], zs[i])
