760317c24eaeb1fb072054ed33499fd845d38548
gaperez64 <gaperez64@gmail.com>
2025-01-27 22:19:46 +0100

    advanced a bit

--- simple_example.jl
+++ simple_example.jl
@@ -32,6 +32,7 @@ function tay_poly(f::Vector{TaylorN{N}}, k::Integer) where {N <: Number}
     return res
 end
 
+
 # Dynamics
 g(y,t) = -y - sin(t) + cos(t)
 
@@ -52,19 +53,31 @@ vals = IntervalBox(1..1, 0..0)
 y, t = set_variables("y t", order=ord)
 
 # Iteration 1
-# Step 0: Taylorize the dynamics
+#
+# Step 0 - Taylorize the dynamics:
 # We want to have a polynomial approximation of the dynamics centered around
 # the midpoint of the current values.
 ytm = TaylorModelN(y, interval(0), IntervalBox(mid(vals)), IntervalBox(domy, 0..1))
 ttm = TaylorModelN(t, interval(0), IntervalBox(mid(vals)), IntervalBox(domy, 0..1))
+# FIXME: Hack to allow for higher degree terms in the intermediate computation
 y, t = set_variables("y t", order=ord*2)
-g_poly_approx = g(ytm, ttm)
-println("Polynomial approximation of the dynamics:")
-println(g_poly_approx)
+gtm = g(ytm, ttm)
+# FIXME: We go back to the lower degree afterwards
+y, t = set_variables("y t", order=ord)
+# We will be needing the highest-degree terms of it too
+htm = TaylorModelN(TaylorN(polynomial(gtm)[ord], ord),
+		   interval(0), expansion_point(gtm), domain(gtm))
 
-# Step 1: Obtain the polynomial part of the Taylor model
-p = tay_poly([polynomial(g_poly_approx)], 4)
-print(p)
+println("Taylor model overapproximation of the dynamics:")
+println(gtm)
+println("Highest degree terms of overapprox:")
+println(htm)
+println("Range bound of the latter:")
+println(quadratic_fast_bounder(htm))
+
+# Step 1 - Obtain the polynomial part of the Taylor model
+p = tay_poly([polynomial(gtm) - polynomial(htm)], 4)
+println(p)
 
 # Step 2: Obtain the remainder/error interval of the TM
 # TODO
