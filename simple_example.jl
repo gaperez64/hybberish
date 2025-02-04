81fde72d191729d10e0f4c634b7b14042d019cda
gaperez64 <gaperez64@gmail.com>
2025-02-04 14:59:38 +0100

    put stuff in a loop

--- simple_example.jl
+++ simple_example.jl
@@ -17,7 +17,8 @@ function tay_poly(f::Vector{TaylorN{N}}, k::Integer) where {N <: Number}
     # variables
     val0 = copy(vars)
     val0[end] = t - t
-    # Prepare a first lie derivative and the result
+    # Prepare a first lie derivative and the result,
+    # in particular we remove the time variable t
     g = copy(vars)
     deleteat!(g, length(vars))
     # Start a vector function for the result
@@ -48,7 +49,12 @@ function picard_tm_extension(vector_field_tms::Vector{T}, tmv, domain, k) where 
     y, t = set_variables("y t", order=k*2)
     ftm = map((fj) -> evaluate(fj, tmv), f)
     y, t = set_variables("y t", order=k)
-    # Add the error of aproximating the true vector field.
+    # Add the error of aproximating the true vector field, here we have NOT
+    # yet reached where we can use formula Xin Chen (p42, antiderivative formula)
+    # we are still just accounting for errors coming from the dynamics having
+    # been approximated by polynomials. Here we're instead using the formula
+    # for the error of TM composition (P, I) \circ (Q, J) = P(Q,J) + I
+    # = (T,K) + I = (T, K + I) 
     add_to_rem(tm, err) = TaylorModelN(polynomial(tm), remainder(tm) + err, tm.x0, domain)
     ftm = add_to_rem.(ftm, errors)
     # Compute the integral's remainder part:
@@ -56,7 +62,7 @@ function picard_tm_extension(vector_field_tms::Vector{T}, tmv, domain, k) where 
     # FIXME: Once again we assume the t interval is the last one
     tdom = domain.v[end]
     # Prepare two functions to define new error intervals pointwise
-    int_enclosure(p_error_terms, dom) = sum(perr(dom) for perr in p_error_terms)
+    int_enclosure(p_error_terms, dom) = sum(evaluate(perr, dom) for perr in p_error_terms)
     pe(tm::TaylorModelN) = polynomial(tm)[k:end]
     return map((tmj) ->
 	       (int_enclosure(pe(tmj), domain) + remainder(tmj)) * tdom, ftm)
@@ -80,43 +86,47 @@ y, t = set_variables("y t", order=ord)
 # Initial state variable bounds and domain
 # y(0) = [1, 1]
 # t(0) = [0, 0]
-vals = IntervalBox(1..1, 0..0)
+vals = IntervalBox(interval(1), interval(0))
 doms = IntervalBox(domy, 0..0.5)
 
-# Iteration 1
-#
-# Step 0: Taylorize the dynamics
-# We want to have a polynomial approximation of the dynamics centered around
-# the midpoint of the current values.
-ytm = TaylorModelN(y, interval(0), vals, doms)
-ttm = TaylorModelN(t, interval(0), vals, doms)
-# FIXME: Hack to allow for higher degree terms in the intermediate computation
-y, t = set_variables("y t", order=ord*2)
-ftm = f_dot(ytm, ttm)
-# FIXME: We go back to the lower degree afterwards
-y, t = set_variables("y t", order=ord)
-println("taylorized vector field/dynamics:")
-println(ftm)
+for _ = 0:2
+	# Iteration 1
+	#
+	# Step 0: Taylorize the dynamics
+	# We want to have a polynomial approximation of the dynamics centered around
+	# the midpoint of the current values.
+	ytm = TaylorModelN(y, interval(0), vals, doms)
+	println("ytm = ")
+	println(ytm)
+	ttm = TaylorModelN(t, interval(0), vals, doms)
+	# FIXME: Hack to allow for higher degree terms in the intermediate computation
+	set_variables("y t", order=ord*2)
+	ftm = f_dot(ytm, ttm)
+	# FIXME: We go back to the lower degree afterwards
+	set_variables("y t", order=ord)
+	println("taylorized vector field/dynamics:")
+	println(ftm)
 
-# Step 1: Obtain the polynomial part of the Taylor model
-p = tay_poly([polynomial(ftm)], ord)
-println("polynomial part of TM:")
-println(p)
+	# Step 1: Obtain the polynomial part of the Taylor model
+	p = tay_poly([polynomial(ftm)], ord)
+	println("polynomial part of TM:")
+	println(p)
 
-# Step 2: Obtain the remainder/error interval of the TM
-# Start Picard iteration, we need the candidate/guessed TM
-candidate_tm = TaylorModelN(p[1], -0.1..0.1, vals, doms)
-# Then we take the TM extension of the approx'd vector field composed with the
-# candidate TM
-rems = picard_tm_extension([ftm], [candidate_tm, ttm], doms, ord)
-println("error interval part of TM:")
-println(rems)
-if all(issubset.(rems, [remainder(candidate_tm)]))
-    println("Contractive!")
+	# Step 2: Obtain the remainder/error interval of the TM
+	# Start Picard iteration, we need the candidate/guessed TM
+	candidate_tm = TaylorModelN(p[1], -0.1..0.1, vals, doms)
+	# Then we take the TM extension of the approx'd vector field composed with the
+	# candidate TM
+	rems = picard_tm_extension([ftm], [candidate_tm, ttm], doms, ord)
+	println("error interval part of TM:")
+	println(rems)
+	if all(issubset.(rems, [remainder(candidate_tm)]))  # FIXME, bugs out here in second iteration
+	    println("Contractive!")
+	end
+
+	# Step 3: Get the new local values (and interval box) and update domain for next step
+	# i.e. just change the domain of the time variable in doms
+	valid_tm = TaylorModelN(p[1], rems[1], vals, doms)
+	global vals = IntervalBox(evaluate(valid_tm, vals), interval(0.5))
+	global doms = IntervalBox(domy, 0.5..1)
 end
-
-# Step 3: Get the new local values (and interval box) and update domain for next step
-# i.e. just change the domain of the time variable in doms
-# TODO:
-# For the new local values, we just need to evaluate the polynomial part of
-# the TM on vals and add the error/remainders. That we can plot!
