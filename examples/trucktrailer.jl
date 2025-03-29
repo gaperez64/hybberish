530b02483c24a5e9a20e383b43b3e8f5fc252d0d
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2025-03-29 15:46:03 +0100

    Update trucktrailer to only work with CSVs.

--- examples/trucktrailer.jl
+++ examples/trucktrailer.jl
@@ -1,6 +1,6 @@
 include("../src/tm_integration.jl")
-
-
+using ArgParse
+using CSV
 
 """Construct the vector field of the given ODEs.
 
@@ -28,22 +28,21 @@ function vector_field!(du::Vector, u::Vector)
 	du[6] = zero(u[6])  # dtheta0 remains constant
 end
 
-"""Run a number of trucktrailer TM integration iterations.
+"""Run one singular trucktrailer TM integration iteration.
 
-    @return The vector of generated initial set rectangles.
-            Return ::Nothing in case of an exception.
+    @return The components (x, y, θ0, θ1, v0, dθ0, t) of the result initial
+            set rectangle. Return ::Nothing in case of an exception.
 """
-function ttintegration(;
-        x::Interval       = 0.00..0.00,
-        y::Interval       = 0.25..0.25,
-        theta0::Interval  = 1.57..1.57,
-        theta1::Interval  = 1.57..1.57,
-        v0::Interval      = 0.10..0.30,
-        dtheta0::Interval = 0.20..0.40,
-        t::Interval       = 0.00..0.00,
+function ttintegration(
+        x::Interval,
+        y::Interval,
+        theta0::Interval,
+        theta1::Interval,
+        v0::Interval,
+        dtheta0::Interval,
+        t::Interval;
         truncation_degree::Integer=4,
-        time_step_size::Float64=0.01,
-        nr_integration_iterations::Integer=1)
+        time_step_size::Float64=0.01)::IntervalBox
     # The truncation degree / the degree of all polynomials
     # that are used during computations.
     ord = truncation_degree
@@ -58,14 +57,11 @@ function ttintegration(;
         ("dtheta0", dtheta0),
         ("t",       t),
     ]
-    init_values = IntervalBox([e[2] for e in initial])
 
     # The fixed time step size.
     tstep::Float64 = time_step_size
-    # The number of TM integration algo iterations.
-    nr_iterations::Integer = nr_integration_iterations
     # Specify time as a finite time horizon.
-    time_horizon::Float64 = nr_iterations * tstep
+    time_horizon::Float64 = 1 * tstep
 
     # The number of times to reattempt the contractiveness test if it fails.
     nr_contractiveness_tries = 10
@@ -104,106 +100,138 @@ function ttintegration(;
         return nothing
     end
 
-    return initial_sets
+    # Only output the final initial set, at the end of the time horizon.
+    return initial_sets[end]
 end
 
-"""Run one singular trucktrailer TM integration iteration.
+"""Call ttintegration for given input and output files.
 
-    @return The components (x, y, θ0, θ1) of the result initial set rectangle.
-            Return ::Nothing in case of an exception.
+    @param[in] ifile The input CSV file path containing points to integrate from.
+    @param[in] ofile The output file path to dump the generated points to.
+    @param[in] tstep The time step size for TM integration.
+    @param[in] rows Process only the first `rows` rows of the input.
+                     If rows is ::Nothing, then process the entire input.
 """
-function ttintegration(
-        x::Interval,
-        y::Interval,
-        theta0::Interval,
-        theta1::Interval,
-        v0::Interval,
-        dtheta0::Interval,
-        t::Interval,
-        time_step_size::Float64)
-    # Run the more general version of this function to generate the
-    # singular initial set rectangle for ONE iteration.
-    boxes = ttintegration(
-        x=x,y=y,theta0=theta0,theta1=theta1,v0=v0,dtheta0=dtheta0,t=t,
-        time_step_size=time_step_size,
-        nr_integration_iterations=1)
+function ttintegration(ipath::String, opath::String, tstep::Float64; rows=nothing)::Nothing
+    # The input file must exist.
+    !isfile(ipath) && return nothing
+    # The output dir must exist, but the output file may not yet.
+    !isdir(dirname(opath)) && return nothing
 
-    if boxes isa Nothing
-        return nothing
+    # Create the output file if it does not exist.
+    touch(opath)
+
+    # Transform each point.
+    reader = CSV.File(ipath)
+    ofile = open(opath, "w")
+
+    # Retain the input header; dump the header of the input file to disk.
+    header = join(string.(propertynames(reader)), ",") * "\n"
+    write(ofile, header)
+
+    # Only process the first `rows` rows of the input.
+    # - `rows = 0` is allowed
+    # - `rows > length(reader)` defaults to `rows = length(reader)`
+    rows = rows === nothing ? length(reader) : min(rows, length(reader))
+
+    for data in reader[1:rows]
+        @assert(length(data) == 8,
+            "The CSV has an unexpected number of columns: got $(length(data)).")
+
+        # Dump the original data point to disk.
+        CSV.write(ofile, [data], writeheader=false, append=true)
+
+        # Unpack the input data to make its column format/ordering explicit.
+        x, y, theta0, theta1, v0, dtheta0, v1, t = data
+
+        combinations = []
+        try
+            # NOTE: Only overwrite variables that change;
+            # v0 and dtheta0 remain unchanged after integration.
+            x, y, theta0, theta1, _, _, t = ttintegration(
+                interval(x), interval(y), interval(theta0), interval(theta1),
+                interval(v0), interval(dtheta0), interval(t),
+                time_step_size=tstep)
+
+            # Assume time is degenerate.
+            t = t.lo
+        catch e
+            # If TM integration fails, just don't generate additional points.
+            continue
+        end
+
+        # Generate one border point using each (inf, mid, sup) combination.
+        product = Iterators.product # An alias.
+        # FIXME: Here we just propagate the input v1. But instead we should
+        #        output the v1 computed during TM integration in the vector
+        #        field???
+        combinations = [
+            [ op1(x), op2(y), op3(theta0), op4(theta1), v0, v1, dtheta0, t ]
+            for (op1, op2, op3, op4) in product(fill([inf, mid, sup], 4)...)
+        ]
+        # Transform a vector of rows into a vector of columns.
+        combinations = [collect(column) for column in zip(combinations...)]
+        # Transform the data to a format that the CSV RowWriter accepts.
+        combinations = NamedTuple([
+            key => val
+            for (key, val) in zip(propertynames(reader), combinations)
+        ])
+
+        # Dump the additionally generated data points to disk.
+        CSV.write(ofile, combinations, writeheader=false, append=true)
     end
-
-    # Extract the components (x, y, θ0, θ1) of the first / resulting
-    # initial set rectangle.
-    return IntervalBox(boxes[1][1:4]...)
 end
 
-"""Parse the ARGS builtin global, expecting all arguments to be intervals
-    of the form a..b where a and b are floats.
-
-    @param[out] input The parsed input intervals. Should be initialized with
-                      default values.
 """
-function parse_input_intervals!(input::Vector{I})::Nothing where {I<:Interval}
-    args = ARGS
+    parse_commandline()
 
-    # There cannot be more input args than the number of variables
-    # that tructrailer supports.
-    if length(ARGS) > length(input)
-        @warn "Truncating redundant input arguments."
-        args = args[1:length(input)]
+    Parse the command-line arguments passed to the program.
+
+    This will return a `Dict` with the following keys:
+    - `debug` a `Bool` flag, if true then output debug logging,
+"""
+function parse_commandline()
+    settings = ArgParseSettings()
+
+    @add_arg_table! settings begin
+        "--debug"
+            help = "Enable/disable debug logging."
+            action = :store_true
+        "tstep"
+            help = "The fixed time step size for TM integration."
+            arg_type = Float64
+            required = true
+        "input"
+            help = "The path to the CSV containing a list of inputs."
+            arg_type = String
+            required = true
+        "output"
+            help = "The path to the output CSV to dump the results in."
+            arg_type = String
+            required = true
+        "rows"
+            help = "Only process the first `rows` rows of the input."
+            arg_type = Int
+            default  = nothing
+            required = false
     end
 
-    # Parse string representations of intervals into actual intervals.
-    #
-    # Strip all whitespace characters.
-    args = [replace(a, ' ' => "") for a in ARGS]
-    # Parse the inputs as intervals.
-    for (idx, a) in enumerate(args)
-        bounds = split(a, "..")
-        @assert(length(bounds) == 2,
-            "Could not split an input on '..' to obtain the lo and hi.")
-        lo, hi = bounds
-        input[idx] = interval(parse(Float64, lo), parse(Float64, hi))
-    end
+    return parse_args(settings)
 end
-
 
 
 
 # If the script is called from the CLI, then run this code.
 if abspath(PROGRAM_FILE) == @__FILE__
 
-    # Specify default values of the variable domains for the CLI.
-    input::Vector{Interval{Float64}} = [
-        0.00..0.00, # x
-        0.25..0.25, # y
-        1.57..1.57, # θ0
-        1.57..1.57, # θ1
-        0.10..0.30, # v0
-        0.20..0.40, # dθ0
-        0.00..0.00  # t
-    ]
+    parsed_args = parse_commandline()
+    display(parsed_args)
 
-    if length(ARGS) > 7
-        @error "Redundant arguments are not allowed, to prevent typos in the "*
-               "input. Specify at most 7 interval arguments. Only leave "*
-               "whitespace between input arguments."
-        exit(1)
-    end
-
-    # Parse the CLI arguments and overwrite zero or more elements of the
-    # default input values.
-    try
-        parse_input_intervals!(input)
-
-        # Run TM integration: obtain the initial set for the first four variables.
-        result = ttintegration(input..., 0.01)
-        @assert result !== nothing "TM integration failed."
-        x, y, theta0, theta1 = result
-        println("(x, y, θ0, θ1) = $((x, y, theta0, theta1))")
-    catch e
-        @error "Input parsing OR TM integration failed: $e"
-        exit(1)
-    end
+    # Handle the file input + file output use case.
+    ipath_ = parsed_args["input"]
+    opath_ = parsed_args["output"]
+    tstep_ = parsed_args["tstep"]
+    rows_ = parsed_args["rows"]
+    ttintegration(ipath_, opath_, tstep_, rows=rows_)
 end
 
