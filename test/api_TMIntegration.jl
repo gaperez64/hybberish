12e074a3c8811b74e84a1986b4f4a6f651f76c81
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2025-04-04 22:18:13 +0200

    Move the preconditioning function.
    Move the preconditioning from 'test/api_TMIntergation.jl'
     to 'src/tm_integration.jl' so that the TM integration
     routine can access it.

--- test/api_TMIntegration.jl
+++ test/api_TMIntegration.jl
@@ -106,155 +106,8 @@ display(polys_lin_map)
 
 
 #
-# TODO: Implement algo 6.1 part 2.
+# The QR preconditioning example from the M. Neher 2006 Taylor models paper.
 #
-
-"""Perform step 2 of Algorithm 6.1 (QR Preconditioned Taylor model method).
-
-    This routine draw from section 3.3.2 of Xin Chen's thesis, and makes use of
-    the references cited there by Xin Chen.
-        1) [MB05]  K. Makino and M. Berz. Suppression of the wrapping effect by
-                   taylor modelbased verified integrators: Long-term
-                   stabilization by preconditioning. International Journal of
-                   Differential Equations and Applications, 10(4):353-384,
-                   2005.
-
-    Also see "6. Preconditioned Quadratic Example." and
-    "Algorithm 6.1 (QR preconditioned Taylor model method)"in the paper:
-        Neher, M. & Jackson, Kenneth & Nedialkov, Nedialko. (2007).
-        On Taylor model based integration of ODEs. SIAM J. Numerical Analysis.
-        45. 236-262. 10.1137/050638448.
-
-    @param[in] tmv_left The left Taylor models that were integrated in step 1.
-    @param[in] tmv_right The right Taylor models that were untouched in step 1.
-    @param[in] vars The space (ODE) variable objects, plus the time variable object.
-    @return The pair of preconditioned left and right Taylor model vectors.
-"""
-function precondition(
-        tmv_left::Vector{TaylorModelN{N,T,S}},
-        tmv_right::Vector{TaylorModelN{N,T,S}},
-        vars::Vector{TaylorN{T}}) where {N,T,S}
-
-    # We rely on unmodified variables to construct linear maps.
-    @assert vars == get_variables() "The variables must be unmodified."
-    @assert length(tmv_left) == length(tmv_right) "Vector length mismatch."
-
-    # Require all domains to be equal within a TaylorModelN vector.
-    # FIXME: `allequal` requires Julia 1.8+. Add an explicit Julia version requirement?
-    @assert allequal(domain.(tmv_left)) "All left TM domains must be equal."
-    @assert allequal(domain.(tmv_right)) "All right TM domains must be equal."
-
-    # Assume the last variable represents time t.
-    t = vars[end]
-    vars_no_t = vars[1:end-1]
-    # NOTE: We assume that neither the left nor right Taylor models contain
-    # time variable t in the polynomial parts. So whatever the value of or
-    # whatever the modifications made to the time domains, it does not affect
-    # the intermediate or final values for preconditioning.
-    timedoml = domain(tmv_left[1]).v[end]
-    unitdom = unitbox(vars)
-
-    # FIXME: Should normalization occur inside this function, or outside?
-    tmv_left = normalize_taylor(tmv_left)
-    # Construct a dummy TM because TM composition requires one TM per variable.
-    ttm = TaylorModelN(t, 0..0, unitdom)
-
-    # (I)
-    # Compute the QR factorization of the linear part of U_{l,j+1}
-    # Each matrix row corresponds to one of the polynomials' linear terms.
-    # FIXME: `stack` requires Julia 1.9+. Add an explicit Julia version requirement?
-    linear_coeffs::Matrix{T} = stack([p[1].coeffs for p in polynomial.(tmv_left)], dims=1)
-    Q::Matrix{T} = Matrix(qr(linear_coeffs).Q)
-
-    # FIXME: Ad hoc: modify the domain of tmv_right in anticipation that the
-    # composition with the QT TMs will induce this domain.
-    tmv_right = [ TaylorModelN(tm, unitdom) for tm in tmv_right ]
-
-    # (II)
-    #
-    # Shift (add) all non-constant terms AND remainders of U_{l,j+1} to U_{r,j}.
-    tmv_shift = tmv_left - constant_polynomial(tmv_left)
-    tmv_right = [ tm(vcat(tmv_right, ttm)) for tm in tmv_shift ]
-    tmv_left -= tmv_shift
-    # The remainders were shifted from left to right, so set left's to zero.
-    # In interval arithmetic generally `[a, b] - [a, b] != [0, 0]`.
-    tmv_left = [ TaylorModelN(tm, 0..0) for tm in tmv_left ]
-
-    # Make Q the linear part of U_{l, j+1}.
-    tmv_left += linear_map(Q, unitdom, vars_no_t)
-
-    # Apply Q^-1 on U_{r,j}.
-    QT::Matrix{T} = transpose(Q)
-    # FIXME: Use an arbitrary domain. See a fixme below for more info.
-    #        The linear map's domain is NOT involved in remainder computation;
-    #        This linear map is just a vehicle for TM evaluation.
-    # FIXME: Don't use arbitrary domain! Use the domain of the left Taylor models
-    # specifically, because Q originates from the left Taylor Models?
-    arbitrary_domain = IntervalBox([tm() for tm in tmv_right]..., ttm())
-    QT_tmv = linear_map(QT, arbitrary_domain, vars_no_t)
-    tmv_right = [ tm(vcat(tmv_right, ttm)) for tm in QT_tmv ]
-
-    # (III)
-    #
-    # Bound the range of the new U_{r,j}.
-    # In other words, modify the domain of the independent variables.
-    # (IV)
-    # Apply a scaling matrix S_(j+1) on U_{r,j} such that each component of the
-    # range of U_{r,j+1} := S_(j+1)^-1 ◦ U_{r,j} is contained in [-1, 1] and
-    # spans [-1, 1] approximately.
-    Sinv = scale(tmv_right)
-    # Convert the matrix to an equivalent form: a linear map Taylor model vector.
-    #=
-        FIXME: We really don't care about the domain this linear map!
-        ==> To satisfy the requirement of the composition "g(f(x))" that
-        the range of "f(x)" must be contained in the domain of "g", we sadly
-        have to provide some arbitrary domain value that satisfies the
-        requirement.
-        ==> Cheat by making the range of the TMs domain of the linear map.
-        The "iscontained" check will exactly check that the range is contained
-        in the domain, so this choice of domain will always work. =#
-    arbitrary_domain = IntervalBox([tm() for tm in tmv_right]..., ttm())
-    Sinv_tmv = linear_map(Sinv, arbitrary_domain, vars_no_t)
-    # Compute U_{r,j+1} := S ◦ U_{r,j} so that each component of the range of
-    # the composition is contained in [-1, 1].
-    # FIXME: But, does the range also span approximately [-1, 1]?
-    tmv_right = [ tm(vcat(tmv_right, ttm)) for tm in Sinv_tmv ]
-
-    # (V)
-    S_ = inv(Sinv)
-    # Convert the matrix to an equivalent form: a linear map Taylor model vector.
-    #=
-        FIXME: Will this linear map domain ever trigger an "iscontained" assertion?
-        The domain of this linear map matters. It will be composed into
-        another Taylor model, so the map will be involved in Taylor model
-        arithmetic and as such influences the computed remainders. =#
-    S_tmv = linear_map(S_, unitdom, vars_no_t)
-    # Set U_{l, j+1} := U_{l, j+1} ◦ S_(j+1)
-    tmv_left = [ tm(vcat(S_tmv, ttm)) for tm in tmv_left ]
-
-    println("Range(tmv_right) ="); display(IntervalBox([tm() for tm in tmv_right]...)); println();
-
-    # Retain the input time components.
-    tmv_left  = [
-        TaylorModelN(tm, IntervalBox(domain(tm).v[1:end-1]..., timedoml))
-        for tm in tmv_left
-    ]
-    tmv_right = [
-        TaylorModelN(tm, IntervalBox(domain(tm).v[1:end-1]..., timedoml))
-        for tm in tmv_right
-    ]
-
-    # For practical purposes, the range of each component of the right
-    # Taylor models should be contained in [-1, 1].
-    # FIXME: Make the check approximate, we need to account for
-    #        over-approximate arithmetic resulting in range bounds
-    #        that are inside [-1, 1] with a small tolerance.
-    @assert all(tm -> isapprox(mag(tm()), 1.0, atol=0.01), tmv_right)
-
-    return tmv_left, tmv_right
-end
-
-
 
 k=5
 set_variables("x y t", order=2*k)
@@ -266,6 +119,8 @@ x, y, t = vars
 # to that time variable t does not appear in the polynomial parts of TMs.
 bogus_time_domain = (-Inf)..(Inf)
 
+# Center the domain on zero.
+# We need to counterbalance this shift when constructing the left initial TMs.
 initial_dom = IntervalBox(0.95..1.05, -1.05..(-0.95), bogus_time_domain)
 initial_mid = mid(initial_dom)
 initial_dom = IntervalBox((initial_dom.v - initial_mid)...)
