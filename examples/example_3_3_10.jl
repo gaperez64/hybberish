a2708103c11230c96e4f3682c97a43094974a9b0
Thomas Gueutal <thomas.gueutal@student.uantwerpen.be>
2025-04-07 23:29:38 +0200

    Update Lotka-Volterra example to use QR preconditioned TM integration.

--- examples/example_3_3_10.jl
+++ examples/example_3_3_10.jl
@@ -1,132 +1,109 @@
-include("plotting.jl")
-include("euler.jl")
-include("scripts/clean_intervals_3_3_10.jl")
-include("tm_integration.jl")
-
-if abspath(PROGRAM_FILE) == @__FILE__
-
-    # Full TM integration, based on Example 3.3.2 and Example 3.3.6
-    k = 3   # The TM arithmetic and truncation order
-    NR_CONTRACTIVENESS_TRIES = 5
-    NR_REFINEMENTS           = 1
-    SCALE                    = 2.0
-    TIME_STEP_SIZE = 0.02
-    TIME_STEP_SIZE_EPS = 2.0e-8  # The minimum time step-size
-    USE_LOCAL_HORIZON = true
-    time_horizon = 0.8     # The finite time horizon
-    vars = set_variables("x t", order=k)
-    vars_no_t = get_variable_names()[1:end-1]
-
-    dom = IntervalBox([-1..1,      # x
-    0..0.02])   # t
-    vars_tms = tm_initial_set(dom)
-    
-    
-    # FIXME: Bumping up the order to avoid assertion errors
-    # during TM arithmetic.
-    old_order = get_order()
-    new_order = old_order*2
-    vars = set_variables(get_variable_string(), order=new_order)
-    
-    # The vector field f of the ODEs:
-    #   f[1] = sin(x)
-    # Represent the vector field as TMs:
-    #   f[1] = x - (1/6)x^3 + I
-    # where I is the Lagrange remainder of approximating
-    # `sin(x)` by `x - (1/6)x^3`
-    f = [ sin(vars_tms[1]) ]   # x
-    # Initial remainder estimate J, a hyperrectangle
-    J = fill(-0.1..0.1, length(f))
-
-    # TODO: Reset to old order.
-    vars = set_variables(get_variable_string(), order=old_order)
-
-    initial_sets, step_sizes =
-        tm_integration(f, dom, k, J, time_horizon,
-                       TIME_STEP_SIZE,
-                       TIME_STEP_SIZE_EPS,
-                       NR_CONTRACTIVENESS_TRIES,
-                       NR_REFINEMENTS,
-                       SCALE)
-    println("\ninit sets ($(length(initial_sets))):")
-    for e in initial_sets
-        println("    $e")
-    end
-    println("\nδi vector ($(length(step_sizes))) = $step_sizes")
-
-	initial_boxes = map((Xi) -> interval_initial_set(Xi, dom), initial_sets)
-	# FIXME: Assume t is the last variable, and drop its dummy vector element.
-	initial_boxes = map((Bij) -> Bij[1:end-1], initial_boxes)
-
-    println("\ninit boxes ($(length(initial_boxes))):")
-    for e in initial_boxes
-        println("    $e")
-    end
-
-    plt3 = plot_boxes_ND(initial_boxes, step_sizes, vars_no_t, use_local_horizon=USE_LOCAL_HORIZON,
-                         title="Computed boxes Bi", titlefontsize=8)
-
-    # Create a sequence of known boxes that have some overlap with
-    # the initial boxes.
-    known_boxes = []
-    for box in initial_boxes
-        known_box = []
-        for interval in box
-            known_int = interval - diam(interval) / 2.0
-            push!(known_box, known_int)
-        end
-        push!(known_boxes, known_box)
-    end
-    known_boxes = get_clean_boxes() # FIXME: insert the clean boxes
-    plt4 = plot_boxes_ND(known_boxes, step_sizes, vars_no_t, use_local_horizon=USE_LOCAL_HORIZON,
-                         title="Given boxes Gi", titlefontsize=8)
-
-    plt5 = nothing
-    if USE_LOCAL_HORIZON
-        # Use a hack to plot TWO sequences of boxes on the same figure:
-        # Append the box vectors to each other. BUT, a shifted version of the
-        # time step vector, since the global time in the plotting function
-        # increases for each box plotted.
-        plt5 = plot_boxes_ND([initial_boxes..., known_boxes...], [step_sizes..., step_sizes...], vars_no_t, use_local_horizon=USE_LOCAL_HORIZON,
-                            title="Bi & Gi")
-    else
-        plt5 = plot_boxes_ND([initial_boxes..., reverse(known_boxes)...], [step_sizes..., (-step_sizes)...], vars_no_t, use_local_horizon=USE_LOCAL_HORIZON,
-                            title="Bi & Gi")
-    end
-
-    intersect_boxes = map(((init, known),) -> intersect(IntervalBox(init), IntervalBox(known)), zip(initial_boxes, known_boxes))
-    intersect_not_empty = map((int) -> emptyinterval() != int, intersect_boxes)
+include("../src/tm_integration.jl")
 
 
-    plt6 = plot_boxes_ND(intersect_boxes, step_sizes, vars_no_t, use_local_horizon=USE_LOCAL_HORIZON,
-                         title="Bi ∩ Gi")
+"""Construct the dynamics.
 
-    figures = [plt3, plt4, plt5, plt6]
-    # Find the single ylim to enclose all plots.
-    ylims_val = hull(Interval.(ylims.(figures))...)
-    ylims_val = (ylims_val.lo, ylims_val.hi)
-    xlims_val = hull(Interval.(xlims.(figures))...)
+    This function is used to:
+        1. Taylorize the dynamics as input to the Lie derivative routine.
+        2. Perform the TM composition in the TM extension of the picard operator.
 
-    # We want there to be overlap between the computed and known boxes.
-    @assert all(intersect_not_empty)
-
-    plt_composed = plot(figures..., layout=(1, length(figures)),
-                        ylims=ylims_val, xticks=[xlims_val.lo, mid(xlims_val), xlims_val.hi])
-
-    euler_step_size = 0.001
-    datax_max = time_horizon
-    tseries, yseries = euler(ode3310!, datax_max, euler_step_size, [0.1])
-    println("euler t: $tseries")
-    println("euler y: $yseries")
-    datax = USE_LOCAL_HORIZON ? [0.0, map((_) -> euler_step_size, tseries[2:end])...] : tseries
-    datay = map((vec) -> vec[1], yseries)
-
-    plot_recursively!(plt_composed, datax, [datay], vars_no_t)
-
-    png_dst = "output/Xi_composed_local_3310.png"
-    println("Press Enter to continue...")
-    display(plt_composed)
-    readline()
-    savefig(plt_composed, png_dst)
-    println("Wrote .png to \"$png_dst\"")
+	@param[out] du The vector field, the right-hand side of the ODEs.
+	@param[in]   u The ODE variables to use in construction.
+"""
+function f_dot!(du::Vector, u::Vector)
+    x, y, t = u
+    du[1] = 1.5*x - x*y
+    du[2] = -3*y  + x*y
 end
+
+
+# The truncation degree / the degree of all polynomials
+# that are used during computations.
+ord = 10
+
+# Initial state variable bounds and domain.
+initial = [
+    ("x", interval(1)),
+    ("y", interval(1)),
+    ("t", interval(0))
+]
+init_values = IntervalBox([e[2] for e in initial])
+
+# The fixed time step size.
+tstep::Float64 = 0.01
+# The number of TM integration algo iterations.
+nr_iterations::Integer = 40
+# Specify time as a finite time horizon.
+time_horizon::Float64 = nr_iterations * tstep
+
+# The scale factor for when contractiveness fails.
+nr_contractiveness_tries = 10
+# The number of refinements to perform at most.
+nr_refinements = 5
+# Quit refinement early if the improvement a single refinement
+# provides falls below this threshold.
+refinement_eps = 0.001
+# The scale factor with which to widen the initial safe remainder
+# estimate when the contractiveness check fails.
+scale_factor = 2.0
+
+
+
+boxes::Vector{IntervalBox} = []
+fboxes::Vector{IntervalBox} = []
+boxes, fboxes = tm_integration_QR(
+    f_dot!,
+    initial,
+    ord,
+    (-0.1..0.1),
+    time_horizon,
+    tstep,
+    nr_contractiveness_tries,
+    nr_refinements,
+    SCALE=scale_factor,
+    REFINEMENT_EPS=refinement_eps
+)
+
+
+
+#
+# PLOTTING
+#
+
+include("../src/euler.jl")
+include("../src/plotting.jl")
+
+
+
+"""Construct the vector field of the given ODEs.
+
+	@param[out] du The vector field, the right-hand side of the ODEs.
+	@param[in]   u The ODE variables to use in construction.
+"""
+function ode_euler!(du, u, p, t)
+	return f_dot!(du, u)
+end
+
+# Start Forward Euler in the middle of the variable domains.
+euler_init_state = Vector(mid(init_values))
+
+# Evaluate Forward Euler.
+euler_step::Float64 = tstep / 10.0
+eseries = euler(ode_euler!, time_horizon, euler_step, euler_init_state)
+
+# Actual plotting
+pltND1 = plot_boxes_ND(boxes, get_variable_names(), sgtitle="initial sets", legend=true)
+pltND2 = plot_boxes_ND(fboxes, get_variable_names(), sgtitle="flowpipes", legend=true)
+
+# Add the Forward Euler curves to the subplots, for each variable.
+for idx in eachindex(pltND1.subplots)
+    plot!(pltND1.subplots[idx], eseries[idx], label="Forward Euler")
+    plot!(pltND2.subplots[idx], eseries[idx], label="Forward Euler")
+end
+pltND = plot(pltND1, pltND2)
+
+println("Show plot ...")
+display(pltND)
+println("Press ENTER to continue.")
+readline()
+println("... done.")
