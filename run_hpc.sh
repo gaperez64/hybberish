21d2647b30f108a17b62ccc23843700ea59cab28
Kasper Engelen <kasper.engelen@hotmail.com>
2025-04-21 18:23:20 +0200

    attempt at running the hybberish on HPC

---
+++ run_hpc.sh
@@ -0,0 +1,23 @@
+#! /bin/zsh
+
+if [ "$#" -ne 2 ] || [ "$1" = "--help" ]; then
+  echo "Run the hybberish utility."
+  echo "Arguments: <experiment name> <node nr 0..7>"
+  exit 1
+fi
+
+cd /project_antwerp/hybberish_code/hybberish/
+
+EXP_NAME=$1
+NODE_NR=$2
+
+julia --project=. -e"using Pkg; Pkg.instantiate();"
+
+for i in $(seq 0 3)
+do
+    FILE_NR=$((NODE_NR * 4 + i))
+    INPUT_FILE="./${EXPERIMENT_NAME}/input/node${NODE_NR}/input_${FILE_NR}.csv"
+    OUTPUT_FILE="./${EXPERIMENT_NAME}/out/output_${FILE_NR}.csv"
+    echo "Processing '${INPUT_FILE}' into '${OUTPUT_FILE}'."
+    julia --project=. ./examples/trucktrailer.jl 0.01 10 "${INPUT_FILE}" "${OUTPUT_FILE}" &
+done
